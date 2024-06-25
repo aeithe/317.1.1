230035f962f9d35974f9787b7f759e46d1068369
pisareva.alisa@gmail.com <pisareva.alisa@gmail.com>
2024-06-25 10:24:09 -0400

    second attempt

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,41 @@
+type Status = "stopped" | "started"
 class Vehicle {
-    status = "stopped";
+  _make:string
+  _model: string
+  _wheels: string | number
+  _status: Status
+    constructor (make:string, model:string, wheels:string | number, status:Status) {
+      this._make = make;
+      this._model = model;
+      this._wheels = wheels;
+      this._status = status;
   
-    constructor(make, model, wheels) {
-      this.make = make;
-      this.model = model;
-      this.wheels = wheels;
     }
     start() {
-      this.status = "started";
+      this._status;
     }
     stop() {
-      this.status = "stopped";
+      this._status;
     }
   }
   
   class Car extends Vehicle {
-    constructor(make, model) {
-      super(make, model, "four");
+    constructor(make, model, wheels, status) {
+      super(make, model, "four", "started");
     }
-  }
+    
+    }
+    function add(x: number | string){
+      if( typeof x ==="string"){
+        this._wheels = +x;
+      }else{
+        return x; 
+      }
+
   
   class MotorCycle extends Vehicle {
-    constructor(make, model) {
-      super(make, model, 2);
+    constructor(make, model, status) {
+      super(make, model, 2, "started");
     }
   }
   
@@ -34,13 +47,13 @@ class Vehicle {
     }
   }
   
-  const myHarley = new MotorCycle("Harley-Davidson", "Low Rider S");
+  const myHarley = new MotorCycle("Harley-Davidson", "Low Rider S", "status");
   myHarley.start();
   printStatus(myHarley);
-  console.log(myHarley.make.toUppercase());
+  console.log(myHarley._make.toLocaleUpperCase());
   
-  const myBuick = new Car("Buick", "Regal");
-  myBuick.wheels = myBuick.wheels - 1;
+  const myBuick = new Car("Buick", "Regal", "status");
+  myBuick._wheels = +myBuick._wheels - 1;
   console.log(myBuick.wheels);
   console.log(myBuick.mdl);
   
